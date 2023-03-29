8a17ba7e25730efda7853de2bb0b953c3a1c0cb4
erikwibowo <erikwibo@gmail.com>
2023-03-30 02:42:09 +0700

    fix locale switcher

--- resources/js/Components/SwitchLocale.vue
+++ resources/js/Components/SwitchLocale.vue
@@ -6,10 +6,10 @@ const lang = usePage().props.app.locale;
 <template>
     <span v-tooltip="'EN/ID'"
         class="inline-flex items-center justify-center rounded text-slate-400 dark:text-slate-500 hover:text-slate-500 dark:hover:text-slate-400 hover:bg-slate-100 dark:hover:bg-slate-900 focus:outline-none focus:bg-slate-100 dark:focus:bg-slate-900 focus:text-slate-500 dark:focus:text-slate-400 transition duration-150 ease-in-out">
-        <Link v-if="lang == 'id'" :href="route('set-locale', 'en')" class="flex items-center space-x-2">
+        <Link v-if="lang == 'id'" :href="route('set-locale', 'en')" class="flex justify-center items-center space-x-2">
         <p class="m-2 w-5 h-5"> ID </p>
         </Link>
-        <Link v-if="lang == 'en'" :href="route('set-locale', 'id')" class="flex items-center space-x-2">
+        <Link v-if="lang == 'en'" :href="route('set-locale', 'id')" class="flex justify-center items-center space-x-2">
         <p class="m-2 w-5 h-5"> EN </p>
         </Link>
     </span>
