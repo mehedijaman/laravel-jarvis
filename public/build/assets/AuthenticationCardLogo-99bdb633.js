8766c28d6667c9e83389061c6047f56a7820783d
erikwibowo <erikwibo@gmail.com>
2023-03-30 15:45:10 +0700

    build

---
+++ public/build/assets/AuthenticationCardLogo-99bdb633.js
@@ -0,0 +1,1 @@
+import{o as a,c as o,w as t,b as e,u as r,i as s}from"./app-da72e2c5.js";import{A as c}from"./ApplicationLogo-be8e7620.js";const f={__name:"AuthenticationCardLogo",setup(n){return(_,i)=>(a(),o(r(s),{href:"/"},{default:t(()=>[e(c,{class:"h-auto w-8"})]),_:1}))}};export{f as _};
