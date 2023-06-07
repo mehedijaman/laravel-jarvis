4b218fd6b01dee276fe3edbd33cad3585439252b
erikwibowo <erikwibo@gmail.com>
2023-06-07 19:44:43 +0700

    api endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/profile', function (Request $request) {
+        return response()->json([
+            'data' => auth()->user(),
+            'message' => "Data user loged in",
+            'status' => Response::HTTP_OK,
+            'access_token' => "",
+            'token_type' => 'Bearer'
+        ]);
+    });
 });
