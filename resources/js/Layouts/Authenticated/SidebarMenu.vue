3660a5c70258421c02484dd0f69afe47571e9422
erikwibowo <erikwibo@gmail.com>
2023-07-27 16:02:42 +0700

    fix: sidebar

--- resources/js/Layouts/Authenticated/SidebarMenu.vue
+++ resources/js/Layouts/Authenticated/SidebarMenu.vue
@@ -25,9 +25,9 @@ import { Link } from "@inertiajs/vue3";
                 </p>
             </Link>
         </div>
-        <ol class="pb-24 pt-6 space-y-2 px-3">
+        <ol class="pb-24 pt-6 space-y-2">
             <li>
-                <span class="font-md uppercase font-semibold">{{
+                <span class="font-md uppercase font-semibold px-3">{{
                     lang().label.main_menu
                 }}</span>
             </li>
@@ -37,7 +37,7 @@ import { Link } from "@inertiajs/vue3";
                         ? 'border-l-4 border-white font-semibold bg-white/20 dark:bg-primary/30'
                         : ''
                 "
-                class="hover:bg-white/20 rounded dark:hover:bg-primary/30"
+                class="hover:bg-white/20 dark:hover:bg-primary/30"
             >
                 <Link
                     :href="route('dashboard')"
@@ -48,7 +48,7 @@ import { Link } from "@inertiajs/vue3";
                 </Link>
             </li>
             <li v-show="can(['user read', 'role read', 'permission read'])">
-                <span class="font-md uppercase font-semibold">{{
+                <span class="font-md uppercase font-semibold px-3">{{
                     lang().label.access
                 }}</span>
             </li>
@@ -59,7 +59,7 @@ import { Link } from "@inertiajs/vue3";
                         ? 'border-l-4 border-white font-semibold bg-white/20 dark:bg-primary/30'
                         : ''
                 "
-                class="hover:bg-white/20 rounded dark:hover:bg-primary/30"
+                class="hover:bg-white/20 dark:hover:bg-primary/30"
             >
                 <Link
                     :href="route('user.index')"
@@ -76,7 +76,7 @@ import { Link } from "@inertiajs/vue3";
                         ? 'border-l-4 border-white font-semibold bg-white/20 dark:bg-primary/30'
                         : ''
                 "
-                class="hover:bg-white/20 rounded dark:hover:bg-primary/30"
+                class="hover:bg-white/20 dark:hover:bg-primary/30"
             >
                 <Link
                     :href="route('role.index')"
@@ -93,7 +93,7 @@ import { Link } from "@inertiajs/vue3";
                         ? 'border-l-4 border-white font-semibold bg-white/20 dark:bg-primary/30'
                         : ''
                 "
-                class="hover:bg-white/20 rounded dark:hover:bg-primary/30"
+                class="hover:bg-white/20 dark:hover:bg-primary/30"
             >
                 <Link
                     :href="route('permission.index')"
@@ -104,7 +104,7 @@ import { Link } from "@inertiajs/vue3";
                 </Link>
             </li>
             <li v-show="can(['setting read'])">
-                <span class="font-md uppercase font-semibold">{{
+                <span class="font-md uppercase font-semibold px-3">{{
                     lang().label.setting
                 }}</span>
             </li>
@@ -115,7 +115,7 @@ import { Link } from "@inertiajs/vue3";
                         ? 'border-l-4 border-white font-semibold bg-white/20 dark:bg-primary/30'
                         : ''
                 "
-                class="hover:bg-white/20 rounded dark:hover:bg-primary/30"
+                class="hover:bg-white/20 dark:hover:bg-primary/30"
             >
                 <Link
                     :href="route('setting.index')"
