b5b166e99c32a7369097836d672724486ece390f
erikwibowo <erikwibo@gmail.com>
2023-07-27 15:50:47 +0700

    fix: navbar

---
+++ public/build/assets/AuthenticationCardLogo-b09df287.js
@@ -0,0 +1,1 @@
+import{o as a,c as o,w as t,b as e,u as r,i as s}from"./app-df339b02.js";import{A as c}from"./ApplicationLogo-87a7a900.js";const f={__name:"AuthenticationCardLogo",setup(n){return(_,i)=>(a(),o(r(s),{href:"/"},{default:t(()=>[e(c,{class:"h-auto w-8"})]),_:1}))}};export{f as _};
